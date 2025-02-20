5cf2c03e1ca7c78fc226e87f840a0d42f45caa98
Shoaib Ahmad <shoaib.ahmad8786@gmail.com>
2025-02-20 23:56:36 +0530

    Making data dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ApName from "./components/AppName";
 import AppTodo from "./components/AppToDo";
-import TodoItem1 from "./components/TodoItem1";
-import TodoItem2 from "./components/TodoItem2";
 import "./App.css";
+import TodoListComps from "./components/TodoList";
 
 function App() {
+  const todoItems = [
+    {
+      name: "Buy Milk",
+      date: "04/11/24",
+    },
+    {
+      name: "Buy Bread",
+      date: "05/11/24",
+    },
+    {
+      name: "Buy Buns",
+      date: "06/11/24",
+    },
+  ];
   return (
     <center className="todo-cont">
       <ApName></ApName>
       <AppTodo></AppTodo>
-      <div className="todo-list">
-        <TodoItem1></TodoItem1>
-        <TodoItem2></TodoItem2>
-      </div>
+      <TodoListComps todoItemsList={todoItems}></TodoListComps>
     </center>
   );
 }
--- src/components/AppToDo.jsx
+++ src/components/AppToDo.jsx
@@ -1,15 +1,15 @@
 function AppTodo() {
   return (
-    <div class="container text-center">
-      <div class="row">
-        <div class="col-6">
+    <div className="container text-center">
+      <div className="row">
+        <div className="col-6">
           <input type="text" placeholder="Enter task" />
         </div>
-        <div class="col-4">
+        <div className="col-4">
           <input type="date" />
         </div>
-        <div class="col-2">
-          <button class="btn btn-success">Add task</button>
+        <div className="col-2">
+          <button className="btn btn-success">Add task</button>
         </div>
       </div>
     </div>
